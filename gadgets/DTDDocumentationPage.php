88fbef3ca1f5e5ae7897af2471e8100d5490218b
euvbonk <seiste@yahoo.de>
2013-03-14 10:17:01 +0000

    DTD-Dokumentation (auf Frames basierend) kann in Verbindung eines Gadget und einem iframe als Seite in gpEasy eingebunden werden. Ausserdem kann die import Funktion jetzt auch bei Pear-Klassennamen mit Asterisk im Pfad umgehen.
    Autoconverted from SVN (revision:41, branch:trunk)

---
+++ gadgets/DTDDocumentationPage.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('is_running') or die('Not an entry point...');
+
+import('de_brb_hvl_wur_stumml_Settings');
+
+class DTDDocumentationPage
+{
+    public function __construct()
+    {
+        $URL = Settings::getHttpUriForFile('xml/dtd_docu/index.html');
+        // Irgendwas
+        echo '<iframe scrolling="auto" height="550px" width="95%" src="'.$URL.'">';
+        echo '<p>Ihr Browser kann leider keine eingebetteten Frames anzeigen:';
+        echo ' Sie k&ouml;nnen die eingebettete Seite &uuml;ber den folgenden Verweis';
+        echo ' aufrufen: <a href="'.$URL.'">DTD-Dokumentation</a></p>';
+        echo '</iframe>';
+    }
+
+    public function DTDDocumentationPage()
+    {
+        self::__construct();
+    }
+}
--- import.php
+++ import.php
@@ -5,47 +5,63 @@ defined('is_running') or die('Not an entry point...');
  * gesamte Addon, da mittels dieser Funktion alle notwendigen Klassen
  * importiert werden, um aufgerufen werden zu können.
  */ 
-if (!function_exists('import')) {
-function import($className)
+if (!function_exists('import'))
 {
-//Directories added here must be
-//relative to the script going to use this file.
-//New entries can be added to this list
-    $directories = array(
-      '',
-      'classes/'
-    );
-
-    //Add your file naming formats here
-    $fileNameFormats = array(
-      '%s.php',
-      '%s.class.php',
-      'class.%s.php',
-      '%s.inc.php',
-      '%s.class.inc'
-    );
-
-    // this is to take care of the PEAR style of naming classes
-    $path = str_ireplace('_', '/', $className);
-    if (include_once($path.'.php'))
+    function import($className)
     {
-        return;
-    }
-   
-    foreach ($directories as $directory)
-    {
-        foreach ($fileNameFormats as $fileNameFormat)
+        //Directories added here must be
+        //relative to the script going to use this file.
+        //New entries can be added to this list
+        $directories = array(
+          '',
+          'classes/'
+        );
+
+        //Add your file naming formats here
+        $fileNameFormats = array(
+          '%s.php',
+          '%s.class.php',
+          'class.%s.php',
+          '%s.inc.php',
+          '%s.class.inc'
+        );
+
+        // this is to take care of the PEAR style of naming classes
+        $path = str_ireplace('_', '/', $className);
+        if (strrpos($className, "*")!== false)
         {
-            $path = $directory.sprintf($fileNameFormat, $className);
-            if (file_exists($path) || is_file($path))
+            $bd = dirname(__FILE__)."/";
+            $su = ".php";
+            foreach (glob($bd.$path.$su) as $directory)
             {
-                require_once($path);
+                $directory = str_replace($bd, "", $directory);
+                $directory = str_replace($su, "", $directory);
+                import($directory);
+            }
+            return;
+        }
+        else
+        {
+            if (include_once($path.".php"))
+            {
                 return;
             }
         }
+
+        foreach ($directories as $directory)
+        {
+            foreach ($fileNameFormats as $fileNameFormat)
+            {
+                $path = $directory.sprintf($fileNameFormat, $className);
+                if (file_exists($path) || is_file($path))
+                {
+                    require_once($path);
+                    return;
+                }
+            }
+        }
     }
-}
-}
+} // end-if
 /*
  * Dies ist der Startpunkt
  * Die Hauptklasse wird importiert. 
@@ -53,6 +69,6 @@ function import($className)
  */
 
 import('de_brb_hvl_wur_stumml_Main');
-import('gadgets_EditorLaunchButton');
+import('gadgets_*');
 
 ?>
